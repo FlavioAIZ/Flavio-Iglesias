b746f2c8e2319bbf8448e84507bd6387d1ae765b
Flavio Iglesias <profe.iglesias.sg@gmail.com>
2024-09-18 23:07:23 -0300

    Update main.php
    Actualización del main.php

--- main.php
+++ main.php
@@ -13,19 +13,40 @@
             $jugador= new jugador($lista[0],$lista[1],$lista[2],$lista[3],$lista[4],$lista[5]);
             $jugadores[]=$jugador;
         }
-fclose($recurso);
+    fclose($recurso);
 
-$ruta = "./2024-TR-equipos.csv";
-$recurso = fopen( $ruta, "r" );
-    $equipos=[];
+    $ruta = "./2024-TR-equipos.csv";
+    $recurso = fopen( $ruta, "r" );
+        $equipos=[];
 
-    while (!feof($recurso))
+        while (!feof($recurso))
+        {
+            $lista=fgetcsv($recurso,1000,",");
+            $equipo= new equipo($lista[0],$lista[1],$lista[2],$lista[3]);
+            $equipos[]=$equipo;
+        }
+    fclose($recurso);
+
+    function jugadorMasAlto($jugadores) :array
     {
-        $lista=fgetcsv($recurso,1000,",");
-        $equipo= new equipo($lista[0],$lista[1],$lista[2],$lista[3]);
-        $equipos[]=$equipo;
+        $maxAltura = 0;
+        $jugadoresMasAltos = [];
+    
+        foreach ($jugadores as $jugador)
+        {
+            if ($jugador->alturaJugador() > $maxAltura) 
+            {
+                $maxAltura = $jugador->alturaJugador();
+                $jugadoresMasAltos = [$jugador->nombreJugador()];
+            } 
+            elseif ($jugador->alturaJugador() === $maxAltura)
+            {
+                $jugadoresMasAltos[] = $jugador->nombreJugador();
+            }
+        }
+        return $jugadoresMasAltos;
     }
-fclose($recurso);
-
-
+    $masalto=[];
+    $masAlto=JugadorMasAlto($jugadores);
+    echo "El jugador mas alto es: ".$masAlto[0],""
 ?>
